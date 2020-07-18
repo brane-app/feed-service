359f08ff07f8a04864a2d0e52398b644741a442c
Noah Harris <dakoolstwunn@gmail.com>
2020-07-18 14:08:19 -0400

    Use monkelib
    Use monkelib for parsing query params

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.13
 
 require (
 	github.com/gastrodon/groudon v0.0.0-20200711221335-e03fb5f2db52
-	github.com/imonke/monkebase v0.0.0-20200715005142-78c2cf6061a4
-	github.com/imonke/monketype v0.0.0-20200713173315-7f5c7c8b4ee9
-	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899 // indirect
+	github.com/imonke/monkebase v0.0.0-20200717040938-ff87d755de3e
+	github.com/imonke/monkelib v0.0.0-20200718153650-0de4bf0665e9
+	github.com/imonke/monketype v0.0.0-20200716234900-cc809fe194ca
 )
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gastrodon/groudon"
 	"github.com/imonke/monkebase"
+	"github.com/imonke/monkelib/middleware"
 
 	"log"
 	"net/http"
@@ -11,7 +12,7 @@ import (
 
 func main() {
 	monkebase.Connect(os.Getenv("MONKEBASE_CONNECTION"))
-	groudon.RegisterMiddleware(contextQueryStrings)
+	groudon.RegisterMiddleware(middleware.RangeQueryParams)
 	groudon.RegisterHandler("GET", "^/all/?", feedAll)
 	http.Handle("/", http.HandlerFunc(groudon.Route))
 	log.Fatal(http.ListenAndServe(":8000", nil))
